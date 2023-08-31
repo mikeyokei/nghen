34e7e4e2ef721676c278c3ee686c8503c3c8c15c
Voth000 <101801239+Voth000@users.noreply.github.com>
2023-08-31 16:27:09 +0700

    fix css

--- src/sites/story/main.js
+++ src/sites/story/main.js
@@ -1,7 +1,20 @@
 var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
+    updateOnWindowResize: true,
     parallax: true,
-    speed: 400,
+    speed: 600,
+    initialSlide: 0,
+    slideToClickedSlide: true,
+ 
 
+    clickable: true,
+});
+
+// Add click event listener to <p> elements
+var paragraphs = document.querySelectorAll('.swiper-slide p');
+paragraphs.forEach(function(paragraph) {
+    paragraph.addEventListener('click', function() {
+        swiper.slideNext(); // Move to the next slide
+    });
 });
