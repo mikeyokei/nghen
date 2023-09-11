f73bfcac30f81a2fa33943d11cd3e530256ee4e3
Voth000 <101801239+Voth000@users.noreply.github.com>
2023-09-11 19:57:09 +0700

    digital

---
+++ src/sites/digital/main.js
@@ -0,0 +1,33 @@
+
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    updateOnWindowResize: true,
+    initialSlide: 0,
+    clickable: true, // Disable slideToClickedSlide
+    noSwiping: true, // Disable swiping
+    noSwipingClass: 'swiper-slide',
+    speed: 0, // Set the speed to 0 for an instant transition
+
+});
+
+
+
+// Add click event listener to elements with class 'cont' (Next button)
+var contButtons = document.querySelectorAll('.cont');
+contButtons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent the button from submitting forms (if any)
+        swiper.slideNext(); // Move to the next slide
+    });
+});
+
+// Add click event listener to elements with class 'back' (Back button)
+var backButtons = document.querySelectorAll('.back');
+backButtons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent the button from submitting forms (if any)
+        swiper.slidePrev(); // Move to the previous slide
+    });
+});
