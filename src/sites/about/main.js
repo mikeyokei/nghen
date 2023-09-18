08a494129ed7ce5fffa7544f5edae089ea746891
Voth000 <101801239+Voth000@users.noreply.github.com>
2023-09-18 22:04:52 +0700

    fix

--- src/sites/about/main.js
+++
@@ -1,7 +0,0 @@
-gsap.fromTo('#cursor', {autoAlpha: 0, x: -20}, {opacity: 0, repeat: -1});
-
-let tween = gsap.to("#typing", {text: {value: " bespoken"}, duration: 1, delay: 1, ease: "none"})
-
-
-
-  
