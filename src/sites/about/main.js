cd0b1aaa07d77296338776d06458de0c22eb38e4
Voth000 <101801239+Voth000@users.noreply.github.com>
2023-09-18 21:57:32 +0700

    stop

--- src/sites/about/main.js
+++ src/sites/about/main.js
@@ -1,4 +1,4 @@
-gsap.fromTo('#cursor', {autoAlpha: 0, x: -20}, {autoAlpha: 1, duration: 0.5, repeat: -1, ease: SteppedEase.config(1)});
+gsap.fromTo('#cursor', {autoAlpha: 0, x: -20}, {opacity: 0, repeat: -1});
 
 let tween = gsap.to("#typing", {text: {value: " bespoken"}, duration: 1, delay: 1, ease: "none"})
 
